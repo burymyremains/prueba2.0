84d3f91afd1c3e1f19fe288ad308e08012aa6002
burymyremains <ehgoe1828@gmail.com>
2025-08-08 14:48:10 -0600

    Tercer commit, cambios menores

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-document.getElementById('actionBtn').addEventListener('click', () => {
-  alert('¡Hola Hugo! Este es tu proyecto con estilo 😎');
+const toggleBtn = document.getElementById('themeToggle');
+
+toggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('manual-dark');
+});
+
+const observer = window.matchMedia('(prefers-color-scheme: dark)');
+observer.addEventListener('change', () => {
+  document.body.classList.remove('manual-dark');
 });
